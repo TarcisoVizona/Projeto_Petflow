d2ffa2ef61e6feb8764d80948599e6a3d9828e88
TarcisoVizona <tarcisovizona@gmail.com>
2025-11-05 15:41:37 -0300

    n2

--- front-end/paginaInicial/paginaInicial.js
+++ front-end/paginaInicial/paginaInicial.js
@@ -1,25 +1,35 @@
-const perfil = document.querySelector("#perfil_link");
-const brinquedos_nav = document.querySelector("#brinquedos_nav");
-const casinhas_nav = document.querySelector("#casinhas_nav");
-const racoes_nav = document.querySelector("#racoes_nav");
-const petiscos_nav = document.querySelector("#petiscos_nav");
-const acessorios_nav = document.querySelector("#acessorios_nav");
+const perfil = document.querySelector("#perfil");
+const logo = document.querySelector("#logo");
+const brinquedos = document.querySelector("#Brinquedos");
+const casinhas = document.querySelector("#Casinhas");
+const racoes = document.querySelector("#Racoes");
+const petiscos = document.querySelector("#Petiscos");
+const acessorios = document.querySelector("#Acessorios");
+const vazia = document.querySelector("#product_cards");
+const imagem_cao = document.querySelector("#image");
+const bolinhas = document.querySelector("#icon_produtos");
+const perfilM = document.querySelector("#perfilM");
+const sair = document.querySelector("#sair");
+const alterarNome = document.querySelector("#alterarNome");
+const input_newname = document.querySelector("#input_newname");
 
 //entrar no perfil
 perfil.addEventListener("click", () => {
-    if (localStorage.getItem("id")) {
-      perfil.href = "../perfil/perfil.html";
-    } else {
-      perfil.href = "file:///C:/Users/Tarciso/Desktop/PetFlow/Projeto/front-end/loginCadastro/login/login.html";
-    }
-  });
+  if (localStorage.getItem("id")) {
+    const nome = document.querySelector("#nome_usuario");
+    const nome_usuario = localStorage.getItem("nomeUsuario");
+    nome.innerHTML = `Olá, ${nome_usuario}!`;
+    perfilM.style.display='flex';
+  } else {
+    window.location.href = "../loginCadastro/login/login.html";
+  }
+});
 
 //mostrar produtos
 window.addEventListener("load", async () => {
-  const div_content = document.querySelector(".product_cards");
   const resposta = await fetch("http://localhost:3000/pagina/inicial");
   const produtos = await resposta.json();
-
+  vazia.innerHTML = "";
   produtos.map((p) => {
     const link = document.createElement("a");
 
@@ -27,7 +37,7 @@ window.addEventListener("load", async () => {
     link.target = "_blank";
 
     const div_card = document.createElement("div");
-    div_card.id = "card";
+    div_card.classList = "card";
 
     const imagem_card = document.createElement("img");
     imagem_card.src = p.img_url;
@@ -43,30 +53,220 @@ window.addEventListener("load", async () => {
     div_card.appendChild(titulo_card);
 
     link.appendChild(div_card);
-
-    div_content.appendChild(link);
+    vazia.appendChild(link);
   });
 });
 
-//link da pagina dos produtos
-brinquedos_nav.addEventListener("click", () => {
-  window.location.href = "../paginasProduto/paginaBrinquedos/brinquedos.html";
+racoes.addEventListener("click", async () => {
+  bolinhas.innerHTML = "";
+  imagem_cao.innerHTML = "";
+  vazia.innerHTML = "";
+  const res = await fetch("http://localhost:3000/produtos/racao");
+  const produtos = await res.json();
+  if (res.status == 200) {
+    produtos.map((p) => {
+      const link = document.createElement("a");
+
+      link.href = `../Detalhes/detalhes.html?id=${p.id_produto}`;
+      link.target = "_blank";
+
+      const div_card = document.createElement("div");
+      div_card.classList = "card";
+
+      const imagem_card = document.createElement("img");
+      imagem_card.src = p.img_url;
+      imagem_card.width = "150";
+      imagem_card.height = "200";
+
+      const preco_card = document.createElement("h5");
+      preco_card.innerText = "R$ " + p.valor_produto;
+
+      const titulo_card = document.createElement("h6");
+      titulo_card.innerText = p.nome_produto;
+
+      div_card.appendChild(imagem_card);
+      div_card.appendChild(preco_card);
+      div_card.appendChild(titulo_card);
+
+      link.appendChild(div_card);
+
+      vazia.appendChild(link);
+    });
+  } else alert("Produtos não encontrados");
 });
 
-casinhas_nav.addEventListener("click", () => {
-  window.location.href = "../paginasProduto/paginaCasinhas/casinhas.html";
+brinquedos.addEventListener("click", async () => {
+  bolinhas.innerHTML = "";
+  imagem_cao.innerHTML = "";
+  vazia.innerHTML = "";
+  const res = await fetch("http://localhost:3000/produtos/brinquedo");
+  const produtos = await res.json();
+  if (res.status == 200) {
+    produtos.map((p) => {
+      const link = document.createElement("a");
+
+      link.href = `../Detalhes/detalhes.html?id=${p.id_produto}`;
+      link.target = "_blank";
+
+      const div_card = document.createElement("div");
+      div_card.classList = "card";
+
+      const imagem_card = document.createElement("img");
+      imagem_card.src = p.img_url;
+      imagem_card.width = "150";
+      imagem_card.height = "200";
+
+      const preco_card = document.createElement("h5");
+      preco_card.innerText = "R$ " + p.valor_produto;
+
+      const titulo_card = document.createElement("h6");
+      titulo_card.innerText = p.nome_produto;
+
+      div_card.appendChild(imagem_card);
+      div_card.appendChild(preco_card);
+      div_card.appendChild(titulo_card);
+
+      link.appendChild(div_card);
+
+      vazia.appendChild(link);
+    });
+  } else alert("Produtos não encontrados");
 });
 
-racoes_nav.addEventListener("click", () => {
-  window.location.href = "../paginasProduto/paginaRacoes/racoes.html";
+acessorios.addEventListener("click", async () => {
+  bolinhas.innerHTML = "";
+  imagem_cao.innerHTML = "";
+  vazia.innerHTML = "";
+  const res = await fetch("http://localhost:3000/produtos/acessorio");
+  const produtos = await res.json();
+  if (res.status == 200) {
+    produtos.map((p) => {
+      const link = document.createElement("a");
+
+      link.href = `../Detalhes/detalhes.html?id=${p.id_produto}`;
+      link.target = "_blank";
+
+      const div_card = document.createElement("div");
+      div_card.classList = "card";
+
+      const imagem_card = document.createElement("img");
+      imagem_card.src = p.img_url;
+      imagem_card.width = "150";
+      imagem_card.height = "200";
+
+      const preco_card = document.createElement("h5");
+      preco_card.innerText = "R$ " + p.valor_produto;
+
+      const titulo_card = document.createElement("h6");
+      titulo_card.innerText = p.nome_produto;
+
+      div_card.appendChild(imagem_card);
+      div_card.appendChild(preco_card);
+      div_card.appendChild(titulo_card);
+
+      link.appendChild(div_card);
+
+      vazia.appendChild(link);
+    });
+  } else alert("Produtos não encontrados");
 });
 
-petiscos_nav.addEventListener("click", () => {
-  window.location.href = "../paginasProduto/paginaPetiscos/petiscos.html";
+casinhas.addEventListener("click", async () => {
+  bolinhas.innerHTML = "";
+  imagem_cao.innerHTML = "";
+  vazia.innerHTML = "";
+  const res = await fetch("http://localhost:3000/produtos/casinha");
+  const produtos = await res.json();
+  if (res.status == 200) {
+    produtos.map((p) => {
+      const link = document.createElement("a");
+
+      link.href = `../Detalhes/detalhes.html?id=${p.id_produto}`;
+      link.target = "_blank";
+
+      const div_card = document.createElement("div");
+      div_card.classList = "card";
+
+      const imagem_card = document.createElement("img");
+      imagem_card.src = p.img_url;
+      imagem_card.width = "150";
+      imagem_card.height = "200";
+
+      const preco_card = document.createElement("h5");
+      preco_card.innerText = "R$ " + p.valor_produto;
+
+      const titulo_card = document.createElement("h6");
+      titulo_card.innerText = p.nome_produto;
+
+      div_card.appendChild(imagem_card);
+      div_card.appendChild(preco_card);
+      div_card.appendChild(titulo_card);
+
+      link.appendChild(div_card);
+
+      vazia.appendChild(link);
+    });
+  } else alert("Produtos não encontrados");
 });
 
-acessorios_nav.addEventListener("click", () => {
-  window.location.href = "../paginasProduto/paginaAcessorios/acessorios.html";
-});
-//link da pagina dos produtos
+petiscos.addEventListener("click", async () => {
+  bolinhas.innerHTML = "";
+  imagem_cao.innerHTML = "";
+  vazia.innerHTML = "";
+  const res = await fetch("http://localhost:3000/produtos/petiscos");
+  const produtos = await res.json();
+  if (res.status == 200) {
+    produtos.map((p) => {
+      const link = document.createElement("a");
 
+      link.href = `../Detalhes/detalhes.html?id=${p.id_produto}`;
+      link.target = "_blank";
+
+      const div_card = document.createElement("div");
+      div_card.classList = "card";
+
+      const imagem_card = document.createElement("img");
+      imagem_card.src = p.img_url;
+      imagem_card.width = "150";
+      imagem_card.height = "200";
+
+      const preco_card = document.createElement("h5");
+      preco_card.innerText = "R$ " + p.valor_produto;
+
+      const titulo_card = document.createElement("h6");
+      titulo_card.innerText = p.nome_produto;
+
+      div_card.appendChild(imagem_card);
+      div_card.appendChild(preco_card);
+      div_card.appendChild(titulo_card);
+
+      link.appendChild(div_card);
+
+      vazia.appendChild(link);
+    });
+  } else alert("Produtos não encontrados");
+});
+
+logo.addEventListener("click", () => {
+  window.location.href = "./paginaInicial.html";
+});
+
+//alterar nome
+alterarNome.addEventListener("click", () => {
+  alterarNome.innerText = "Salvar";
+  input_newname.style.display = "block";
+
+  alterarNome.addEventListener("click", async () => {
+    const nomeUsuario = input_newname.value;
+    localStorage.setItem("nomeUsuario", nomeUsuario);
+    window.location.reload();
+  });
+  return;
+});
+
+//logout
+sair.addEventListener("click", () => {
+  localStorage.removeItem("email");
+  localStorage.removeItem("id");
+  window.location.href = "../paginaInicial/paginaInicial.html";
+});
