80c87203c74b0d7d6bcf53262a301a9528e1e0ee
TarcisoV <137207600+TarcisoVizona@users.noreply.github.com>
2025-11-07 16:03:43 -0300

    Delete admin.js

--- admin.js
+++
@@ -1,6 +0,0 @@
-const sair = document.querySelector(".sair");
-sair.addEventListener("click", () => {
-  localStorage.removeItem("email");
-  localStorage.removeItem("id");
-  window.location.href = "../paginaInicial/paginaInicial.html";
-});
